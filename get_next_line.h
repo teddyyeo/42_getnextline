02505f6c2707395640ebb9426a7f90c49983fecf
tayeo <tayeo@student.42seoul.kr>
2022-07-06 19:14:48 +0900

    norm for header

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: tayeo <tayeo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 16:09:21 by tayeo             #+#    #+#             */
-/*   Updated: 2022/07/06 19:12:00 by tayeo            ###   ########.fr       */
+/*   Updated: 2022/07/06 19:14:12 by tayeo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
+
 char *get_next_line(int fd);
 #endif
